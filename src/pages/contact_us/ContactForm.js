76390cb78f2b889090eb0ed505c9873840811e5c
toni-carlson <66501530+toni-carlson@users.noreply.github.com>
2024-01-02 22:59:20 -0500

    Filled in contact us page

---
+++ src/pages/contact_us/ContactForm.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './ContactUs.css';
+
+function ContactForm() {
+   return (
+        <form target="_blank" action="https://formsubmit.co/tonicarlson13@gmail.com" method="POST">
+            <div className="form-input">
+                <label htmlFor="form-filler-name">Your name</label>
+                <input id="form-filler-name" type="text" name="name" placeholder="Your name" required />
+            </div>
+            <div className="form-input">
+                <label htmlFor="contact-email">Email</label>
+                <input id="contact-email" type="email" name="email" placeholder="Email" required/>
+            </div>
+            <div className="form-input">
+                <label htmlFor="contact-phone">Phone</label>
+                <input id="contact-phone" type="phone" name="phone" placeholder="Phone" required/>
+            </div>
+            <div className="form-input">
+                <label htmlFor="client-name">Who would we be treating?</label>
+                <input id="client-name" type="text" name="clientname" placeholder="Potential client name" required />
+            </div>
+            <div className="form-input">
+                <label htmlFor="message-textarea">Please tell us why you're interested in seeing us</label>
+                <textarea 
+                    id="message-textarea" 
+                    placeholder="Be as vague or specific as you'd like!" 
+                    name="message" 
+                    rows="10" 
+                    required
+                ></textarea>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+   );
+}
+
+export default ContactForm;
--- src/pages/contact_us/ContactUs.js
+++ src/pages/contact_us/ContactUs.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import './ContactUs.css';
+import ContactForm from './ContactForm';
 import FrameLayout from '../../frame_layout/FrameLayout';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope, faMap } from '@fortawesome/free-solid-svg-icons';
 
 function ContactUs() {
-  return (
+   return (
       <FrameLayout>
-         <h1>
-            Contact Us
-         </h1>
+         <div className="two-col-display">
+            <div className="left-col">
+               <h1>
+                  Contact Us
+               </h1>
+               <p>
+                  Integrative Mind Connections is located in Brownsburg, Indiana on the west side of Indianapolis 
+                  just off I-74 in the Data Comm building. Contact us today to schedule your FREE in office consultation. 
+               </p>
+               <div className='cursive-font'>Your better tomorrow begins today!</div>
+               <br />
+               <ContactForm />
+            </div>
+            <div className="right-col">
+               <h2>Contact Information</h2>
+               <ul className="contact-info-list">
+                  <li className="fusion-li-item">
+                     <FontAwesomeIcon icon={faPhone} />
+                     <span>(317)339-4998</span>
+                  </li>
+                  <li className="fusion-li-item">
+                     <FontAwesomeIcon icon={faEnvelope} />
+                     <a href="mailto:integrative.mind.conn@gmail.com" target="_blank" rel="noopener noreferrer">
+                        integrative.mind.conn@gmail.com
+                     </a>
+                  </li>
+                  <li className="fusion-li-item">
+                     <FontAwesomeIcon icon={faMap} />
+                     <span>
+                        680 Patrick Place <br />
+                        Brownsburg, Indiana 46112
+                     </span>
+                  </li>
+               </ul>
+            </div>
+         </div>
       </FrameLayout>
-  );
+   );
 }
 
 export default ContactUs;
