23cb6db2100e9635dedb84ac19599ee9b13b2370
toni-carlson <66501530+toni-carlson@users.noreply.github.com>
2024-01-06 21:08:44 -0500

    Updated styling based on discussion with client

--- src/pages/home_page/HomePageHeader.js
+++ src/pages/home_page/HomePageHeader.js
@@ -4,7 +4,8 @@ import PageHeader from '../../page_header/PageHeader.js';
 import { orgName } from '../../constants';
 
 function HomePageHeader() {
-  const orgNameArray = orgName.split(' ');
+  let [orgNameStart, ...orgNameEnd] = orgName.split(' ');
+  orgNameEnd = orgNameEnd.join(' ');
 
   return (
     <div className="section header">
@@ -12,9 +13,9 @@ function HomePageHeader() {
         <PageHeader />
         <div className="header-content">
             <div className="cursive-font welcome">Welcome to</div>
-            <h1 className="welcome-message">
-              { orgNameArray[0] }<br/>
-              { orgNameArray[1] }&nbsp;{ orgNameArray[2] }
+            <h1 className="welcome-org-name">
+              { orgNameStart }<br/>
+              { orgNameEnd }
             </h1>
             <div className="cursive-font small">
                 Repairing&nbsp;minds. Rebuilding&nbsp;lives. Restoring&nbsp;hope.
