adcd2141d1dd80fd2760080449c6d25d56a24336
toni-carlson <66501530+toni-carlson@users.noreply.github.com>
2024-02-25 23:34:34 -0500

    Temporarily removed nav links for whatweoffer & whoweare

--- src/page_header/NavLinks.js
+++ src/page_header/NavLinks.js
@@ -5,8 +5,8 @@ function NavLinks({ closeMenu }) {
     <>
       <Link to='/' onClick={closeMenu}>Home</Link>
       <Link to='/whowehelp' onClick={closeMenu}>Who we help</Link>
-      <Link to='/whatweoffer' onClick={closeMenu}>What we offer</Link>
-      <Link to='/whoweare' onClick={closeMenu}>Who we are</Link>
+      {/* <Link to='/whatweoffer' onClick={closeMenu}>What we offer</Link>
+      <Link to='/whoweare' onClick={closeMenu}>Who we are</Link> */}
       <Link to='/contactus' onClick={closeMenu}>Contact us</Link>
     </>
   );
