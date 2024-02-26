a5a15aeb2362e5141854eee9fa7e75c19db21842
toni-carlson <66501530+toni-carlson@users.noreply.github.com>
2024-02-25 23:20:56 -0500

    Autoclose mobile menu

--- src/page_header/MobileHeaderNavigation.js
+++ src/page_header/MobileHeaderNavigation.js
@@ -5,6 +5,7 @@ import NavLinks from './NavLinks';
 import './MobileHeaderNavigation.css';
 
 function MobileHeaderNavigation() {
+    const container = useRef(null);
     const [isOpen, setIsOpen] = useState(false);
 
     function toggleMenu() {
@@ -13,8 +14,23 @@ function MobileHeaderNavigation() {
         }, 50);
     }
 
+    if (container.current && isOpen) {
+        document.addEventListener('click', closeMenu);
+        container.current.addEventListener('focusout', closeMenu);
+    } else {
+        document.removeEventListener('click', closeMenu);
+        container.current?.removeEventListener('focusout', closeMenu)
+    }
+
+    function closeMenu(e) {
+        const hasFocusLeft = e.relatedTarget && !container.current?.contains(e.relatedTarget);
+        if (!container.current?.contains(e.target) || hasFocusLeft) {
+            toggleMenu();
+        }
+    }
+
     return (
-        <nav className='top-navigation-mobile' aria-label='main'>
+        <nav className='top-navigation-mobile' aria-label='main' ref={container}>
             <button type='button' onClick={toggleMenu} aria-label='Menu' aria-expanded={isOpen} aria-controls='mobile-nav'>
                 <FontAwesomeIcon icon={faBars} />
             </button>
